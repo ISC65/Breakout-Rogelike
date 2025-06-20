f48eb3ed1ece9e0bfbc9dcd0d554e82bcc64d533
ISC65 <shonajns22@googlemail.com>
2025-06-20 21:20:34 +0100

    Update brick.gd

--- a-rougelike-breakout-game/sprite_scripts/brick.gd
+++ a-rougelike-breakout-game/sprite_scripts/brick.gd
@@ -1,6 +1,15 @@
 extends StaticBody2D
 class_name Brick
 
+var types = [
+	"big",
+	"normal",
+	"immune",
+	"explosive",
+	"brick",
+	"wet"
+]
+
 @onready var animatedself: AnimatedSprite2D = $Brick
 
 func breakself():
